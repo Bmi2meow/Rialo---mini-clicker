115a7ffcbb01948d1b582038c7a4e43cebec3e27
Bmi2meow <118408942+Bmi2meow@users.noreply.github.com>
2025-09-29 14:27:40 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,9 @@
+const btn   = document.getElementById('btn');
+const scoreEl = document.getElementById('score');
+let score = 0;
+
+btn.addEventListener('click', () => {
+    score++;
+    scoreEl.textContent = score;
+});
 
